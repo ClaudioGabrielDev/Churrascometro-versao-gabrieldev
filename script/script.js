b95770dcc6f39c6d50b2620026b3588b74f66b75
Claudio Gabriel <designgabriel15@gmail.com>
2021-01-08 20:21:29 -0300

    Estruturando o projeto
    Criando os arquivos: Index.html; style.css && script.js - e estabelecendo regra de negocio

---
+++ script/script.js
@@ -0,0 +1,10 @@
+/*   
+    - Carne: 400gm por Adulto; + de 6 horas = 650gr
+    - Cerveja: 1200ml por Pessoa; + de 6 horas = 2000ml
+    - Refrigerante e Água: 1000ml por Pessoa; + de 6 horas = 1500ml
+
+    - Crianças valem 0.5
+
+*/
+
+alert("Olá, Calcule aqui o seu churrasco futuramente!")
